a1527a1f4fab9b4d3d29479d5224ba8571d6e540
Seshan Perera <seshanperera2004@gmail.com>
2026-06-23 02:07:27 +0530

    Adjust opacity transform thresholds in HeroSection

--- project/components/HeroSection.tsx
+++ project/components/HeroSection.tsx
@@ -25,7 +25,7 @@ export default function HeroSection() {
     offset: ['start start', 'end start'],
   });
   const y = useTransform(scrollYProgress, [0, 1], ['0%', '20%']);
-  const opacity = useTransform(scrollYProgress, [0.1, 0.3], [1, 0]);
+  const opacity = useTransform(scrollYProgress, [0.2, 0.4], [1, 0]);
 
   return (
     <section
